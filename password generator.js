dd6cfa799aa9538f7527e2748b0f7cb71bb996b2
Ari Tserendavaa <tserenda@hotmail.com>
2019-12-16 18:55:58 -0800

    implemented the logic

--- password generator.js
+++ password generator.js
@@ -14,6 +14,7 @@ function updateHtml() {
   const hr = document.createElement("hr");
   div.appendChild(hr);
   const textarea = document.createElement("textarea");
+  textarea.readOnly = true;
   textarea.setAttribute("id", "textarea");
   textarea.setAttribute("placeholder", "Your Secure Password");
   div.appendChild(textarea);
@@ -30,3 +31,46 @@ function updateHtml() {
   div.appendChild(button2);
   document.body.appendChild(div);
 }
+
+const generateButton = document.querySelector("#generate");
+const copyButton = document.querySelector("#copy");
+
+generateButton.addEventListener("click", function() {
+  var passwordLength = 0;
+  while (passwordLength < 8 || passwordLength > 128) {
+    passwordLength = prompt("Enter password length between 8 and 128");
+  }
+
+  var [includeSpecialChars, includeUppercaseChars, includeLowercaseChars, includeDigits] = [false, false, false, false];
+  while (!includeSpecialChars && !includeUppercaseChars && !includeLowercaseChars && !includeDigits) {
+    includeSpecialChars = confirm("Include special characters?");
+    includeUppercaseChars = confirm("Include UPPERCASE?");
+    includeLowercaseChars = confirm("Include lowercase?");
+    includeDigits = confirm("Include digits?");
+  }
+
+  var charSet = "";
+  if (includeUppercaseChars) charSet += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  if (includeLowercaseChars) charSet += "abcdefghijklmnopqrstuvwxyz";
+  if (includeSpecialChars) charSet += "!#$%&'()*+,-./:;<=>?@{|}~";
+  if (includeDigits) charSet += "0123456789";
+
+  var password = "";
+  for (let i = 0; i < passwordLength; i++) {
+    const randomIndex = (Math.random() * 100) % charSet.length;
+    password += charSet.charAt(randomIndex);
+  }
+  const textAreaElement = document.querySelector("textarea");
+  textAreaElement.textContent = password;
+  copyButton.disabled = false;
+});
+
+copyButton.addEventListener("click", function() {
+  const password = document.querySelector("textarea");
+  password.select();
+  password.setSelectionRange(0, 99999);
+  document.execCommand("copy");
+  alert("Password copied:\n" + password.value);
+  password.textContent = "";
+  this.disabled = true;
+});
